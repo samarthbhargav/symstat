6a2a60f958cc27dcd20cfdedb45097e099621451
samarthbhargav <samarth.bhargav92@gmail.com>
2019-05-30 17:16:57 +0200

    Added balanced samples for f-MNIST

--- fashion_mnsit.py
+++ fashion_mnsit.py
@@ -9,6 +9,28 @@ from torchvision import datasets, transforms
 log = logging.getLogger(__name__)
 
 
+def balanced_batches(dataset, batch_size):
+    unlabled_idx = dataset.unlabeled_idx
+    labeled_idx = list(filter(lambda _: _ not in unlabled_idx,
+                              np.arange(len(dataset.targets))))
+    labeled_idx = np.array(labeled_idx)
+
+    # construct batches - half of them should be from unlabled, half from labeled
+    n_batches = (len(unlabled_idx) // (batch_size//2)) + 1
+    for ulb in np.array_split(unlabled_idx, n_batches):
+        batch_idx = list(ulb)
+        lb = np.random.choice(labeled_idx, size=(
+            batch_size // 2), replace=True)
+        batch_idx.extend(lb)
+        x_batch = []
+        y_batch = []
+        for idx in batch_idx:
+            x, y = dataset[idx]
+            x_batch.append(x)
+            y_batch.append(y)
+        yield torch.stack(x_batch), torch.LongTensor(y_batch)
+
+
 class FashionMNIST(datasets.FashionMNIST):
 
     UNLABLED = -1
@@ -66,14 +88,14 @@ if __name__ == '__main__':
 
         break
 
-    fmnist = FashionMNIST("./fashion-mnist", 0.0, train=False,
+    fmnist = FashionMNIST("./fashion-mnist", 0.998, train=True,
                           transform=fmnist_transforms, download=True)
 
-    for x_raw, y_raw in DataLoader(fmnist, batch_size=10):
-        x, y, x_unlab, y_unlab = FashionMNIST.separate_unlabeled(x_raw, y_raw)
+    for x, y in balanced_batches(fmnist, 16):
+        print(x.size(), y)
+    # for x_raw, y_raw in DataLoader(fmnist, batch_size=10):
+    #     x, y, x_unlab, y_unlab = FashionMNIST.separate_unlabeled(x_raw, y_raw)
 
-        print(y, y_unlab)
-        print(x.size(), y.size())
-        print(x_unlab.size(), y_unlab.size())
-
-        break
+    #     print(y, y_unlab)
+    #     print(x.size(), y.size())
+    #     print(x_unlab.size(), y_unlab.size())
