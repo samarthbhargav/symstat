db640a4d998d841236597236ee8af9d6a0728e29
akashrajkn <akashrajkn@gmail.com>
2019-05-30 16:58:32 +0200

    Sem loss func. is fixed. Horrray

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from collections import OrderedDict
 
 import torch
 from torch import nn
+import torch.nn.functional as F
 
 log = logging.getLogger(__name__)
 
@@ -28,7 +29,7 @@ def get_layers(n_classes):
     layers["relu_2"] = nn.ReLU(inplace=True)
 
     layers["fc_3"] = nn.Linear(128, n_classes)
-    layers["log_sf"] = nn.LogSoftmax(dim=1)
+    # layers["log_sf"] = nn.LogSoftmax(dim=1)
     return nn.Sequential(layers)
 
 
@@ -37,38 +38,54 @@ class SemanticLossModule(nn.Module):
         super().__init__()
         self.device = device
         self.layers = get_layers(n_classes)
+        self.n_classes = n_classes
         self.w_s    = args.w_s
 
         log.info("Classifier: {}".format(self.layers))
-        self.criterion = nn.NLLLoss()
+        self.criterion = nn.BCEWithLogitsLoss()
 
     def forward(self, x):
         return self.layers(x)
 
+    def pack_y(self, y):
+        yy = torch.zeros(y.size(0), self.n_classes)
+        for idx, _ in enumerate(y):
+            yy[idx, _] = 1.0
+        return yy
+
     def compute_loss(self, x, y, x_unlab, u_unlab):
+
+        loss = 0.
+        sl   = 0.
 
         if x.size(0) > 0:
             out = self(x)
-            loss = self.criterion(out, y)
-        else:
-            loss = 0.
+            loss = self.criterion(out, self.pack_y(y))
 
         if x_unlab.size(0) > 0:
             out_unlab = self(x_unlab)
-            sl = self.compute_semantic_loss(out_unlab)
-        else:
-            sl = 0.
+            sl = torch.mean(self.sem_loss(torch.sigmoid(out_unlab)))
 
-        return loss + self.w_s * sl
+        return (x.size(0) * loss + x_unlab.size(0) * sl) / (x.size(0) + x_unlab.size(0))
+        # return sl
 
-    def compute_semantic_loss(self, probs):
+    def sem_loss(self, probs):
+        s = torch.zeros(probs.size(1))
+
+        for i, p_i in enumerate(probs):
+            s_part = p_i
+            for j, p_j in enumerate(probs):
+                if i == j:
+                    continue
+                s_part = torch.mul(s_part, (1 - p_j))
+            s = torch.add(s, s_part)
+            # s += s_part
+        return -1 * torch.log(s)
+
+    def compute_semantic_loss(self, norm_probs, num_classes=10):
         '''
         Return semantic loss for exactly one constraint
         '''
-        # num_classes   = probs.size()[0]
-        num_classes   = 10
-
-        norm_probs    = torch.sigmoid(probs)
         semantic_loss = torch.tensor(0.)
 
         for i in range(num_classes):
@@ -77,7 +94,5 @@ class SemanticLossModule(nn.Module):
             one_situation    = torch.tensor(one_situation)
 
             semantic_loss   += torch.prod(one_situation - norm_probs)
-
-        semantic_loss = torch.clamp(semantic_loss, min=1e-5)
 
         return -torch.log(torch.abs(semantic_loss))
