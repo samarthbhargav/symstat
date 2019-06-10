2b7bd6b010feb8ac6bdba94a9ec508d6049dd14e
akashrajkn <akashrajkn@gmail.com>
2019-06-10 21:39:51 +0200

    ipynb books backup

--- run_script.sh
+++ run_script.sh
@@ -2,4 +2,4 @@
 
 MODELID=1
 
-python symstat.py train --model=sl --model-id=$MODELID --num-workers=4 --unlabeled=0.998 --batch-size=32  --epochs=100
+python symstat.py train --model=sl --model-id=$MODELID --num-workers=4 --unlabeled=0.998 --batch-size=128  --epochs=100
